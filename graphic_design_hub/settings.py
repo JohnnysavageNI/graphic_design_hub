d895ee25b6105f7eaa655c53f00aa6ad3a33cddd
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-07-24 19:44:13 +0100

    added home app and templates

--- graphic_design_hub/settings.py
+++ graphic_design_hub/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    'home',
+
 ]
 
 MIDDLEWARE = [
@@ -58,7 +61,10 @@ ROOT_URLCONF = 'graphic_design_hub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

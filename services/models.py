39c6c6f9f772d5e2f0abe59505bcfed3168c6581
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-08-19 20:03:15 +0100

    added images to services

--- services/models.py
+++ services/models.py
@@ -5,6 +5,7 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.ImageField(upload_to="service_images/", blank=True, null=True)
     stripe_price_id = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
 

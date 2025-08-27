ebcf9390eb7ab77de8d1f2f3c0ce209e776638af
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-08-27 21:42:04 +0100

    more settings to fix static

--- graphic_design_hub/settings.py
+++ graphic_design_hub/settings.py
@@ -211,13 +211,15 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+USE_AWS = os.getenv("USE_AWS", "false").lower() in {"1", "true", "yes"}
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'graphicdesignhub'
-    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'

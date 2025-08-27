d25045d0355c52891275b4823ab229afaad4b8ad
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-08-27 21:11:19 +0100

    more settings changes

--- custom_storages.py
+++ custom_storages.py
@@ -3,11 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    default_acl = None
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    default_acl = None
-    file_overwrite = False
     location = settings.MEDIAFILES_LOCATION

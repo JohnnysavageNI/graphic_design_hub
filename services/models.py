872f4d1aa1fe1a719c1fde42a76b7ab4685b0bf4
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-08-28 20:10:51 +0100

    update services model

--- services/models.py
+++ services/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from custom_storages import MediaStorage
 
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    image = models.ImageField(upload_to="service_images/", blank=True, null=True)
+    image = models.ImageField(
+        storage=MediaStorage(),
+        upload_to="service_images/", 
+        blank=True, 
+        null=True
+    )
     stripe_price_id = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
 
